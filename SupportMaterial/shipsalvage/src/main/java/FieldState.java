c3162303101f1cbc5d120df88606a06a0bae19c8
Fabian Koch <Fabian Koch>
2023-05-14 21:24:06 +0200

    a: FieldState enum

---
+++ SupportMaterial/shipsalvage/src/main/java/FieldState.java
@@ -0,0 +1,29 @@
+import com.google.common.cache.Cache;
+
+public enum FieldState {
+    // initialize Field States
+    EMPTY(' '),
+    MISS('X'),
+    OCCUPIED_HIDDEN('O'),
+    OCCUPPIED_SALVAGED('#');
+
+    private final char output;
+    
+    private FieldState(char output) {
+        this.output = output;
+    }
+
+    public static FieldState fromOutput(char output) {
+        try {
+            // find corrisponding FieldState to output
+            for (FieldState state : FieldState.values()) {
+                if (state.output == output)
+                    return state;
+            }
+        } catch (Exception e) { // TODO: Specific exception
+            e.printStackTrace();
+            // TODO: IllegalArgumentException
+        } 
+        return null;
+    }
+}
