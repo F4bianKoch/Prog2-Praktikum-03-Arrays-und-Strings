c527baca1be1bd6337d975cb3dfce2fdbf07b451
Fabian Koch <fabian.cook@web.de>
2023-06-12 12:51:40 +0200

    a: shipsalvage done without randome genrator

--- SupportMaterial/shipsalvage/src/main/java/ShipSalvage.java
+++ SupportMaterial/shipsalvage/src/main/java/ShipSalvage.java
@@ -1,21 +1,32 @@
+import java.util.Scanner;
+
 public class ShipSalvage {
 
   private static final char[][] exampleMap = new char[][]
   {
-    {'O','O', ' ', 'O', ' ', ' ', 'O', 'O', 'O', 'O'},
-    {' ',' ', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' '},
-    {'O',' ', ' ', 'O', ' ', 'O', 'O', 'O', ' ', 'O'},
-    {'O',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'O'},
-    {'O',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {'O',' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O'},
-    {' ',' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O'},
-    {'O','O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'O'},
+    {'O','O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
     {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ',' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O'},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
   };
 
   public static void main(String[] args) {
-    printMap(getExample(), true);      
+    Scanner input = new Scanner(System.in);
+    FieldState[][] map = getExample();
+
+    while (!allSalvaged(map)) {
+      probeField(map, input.next());
+      printMap(map, false);
+    }
+
+    input.close();
+    System.out.println("Alle Schiffe geborgen!");
   } 
 
   public static FieldState[][] getExample() {
@@ -54,11 +65,14 @@ public class ShipSalvage {
   public static void printMap(FieldState[][] map, boolean showHidden) {
     checkValidMap(map);   // validate map
 
-    System.out.println(" ABCDEFGHIJ"); 
-    System.out.println("+-----------+");
+    System.out.println("   ABCDEFGHIJ"); 
+    System.out.println("+------------+");
 
     for (int i = 0; i < map.length; i++) {
-      System.out.print(i + "|");
+      if (i + 1 < 10)
+        System.out.print((i + 1) + " |");
+      else
+        System.out.print((i + 1) + "|");
 
       for (int j = 0; j < map[i].length; j++) {
         char value = map[i][j].getOutput();
@@ -66,7 +80,7 @@ public class ShipSalvage {
         if (showHidden) {
           System.out.print(value);
         } else {
-          if (value != '#')
+          if (value != 'O')
             System.out.print(value);
           else
             System.out.print(' ');
@@ -89,6 +103,43 @@ public class ShipSalvage {
       }
     }
     return true;
+  }
+
+  public static void probeField(FieldState[][] map, String field) {
+    checkValidMap(map);
+    String[] chars = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+    if(field.matches("([A-J,a-j][1-9])") || field.matches("([A-J,a-j][1][0])")){
+      int col = 0;
+
+      for (String character : chars) {
+        if (field.toUpperCase().contains(character))
+          break;
+        col++;
+      }
+
+      switch (map[Integer.parseInt(field.substring(1)) - 1][col]) {
+        case EMPTY:
+          map[Integer.parseInt(field.substring(1)) - 1][col] = FieldState.MISS;
+          System.out.println("Nichts zu finden!");
+          break;
+        case OCCUPIED_HIDDEN:
+          map[Integer.parseInt(field.substring(1)) - 1][col] = FieldState.OCCUPPIED_SALVAGED;
+          System.out.println("Wrack gefunden!");
+          break;
+        case OCCUPPIED_SALVAGED:
+          map[Integer.parseInt(field.substring(1)) - 1][col] = FieldState.OCCUPPIED_SALVAGED;
+          System.out.println("Bereits untersucht!");
+          break;
+        case MISS:
+          map[Integer.parseInt(field.substring(1)) - 1][col] = FieldState.MISS;
+          System.out.println("Bereits untersucht!");
+          break;
+      }
+
+    } else {
+      System.out.println("Fehlerhafte Eingabe!");
+    }
   }
 
 }
